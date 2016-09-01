2e5f9075c4cab10cbf9690cafe8a6d33b2cb7fda
walkermannum1 <2469377884@qq.com>
2016-09-01 18:11:01 +0800

    Make a click listener and sava state.

--- app/src/main/java/com/example/user/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/user/criminalintent/CrimeActivity.java
@@ -1,14 +1,24 @@
 package com.example.user.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+
+import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
+    private static final String EXTRA_CRIME_ID = "com.example.user.criminalintent.crime_id";
+
+    public static Intent newIntent(Context pakageContext, UUID crimeId) {
+        Intent intent = new Intent(pakageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
